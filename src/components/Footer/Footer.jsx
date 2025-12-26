d1c4a7450e6ed3beb0501487d18b4f3b1c4599bb
waqqascalaveras <waqqascalaveras@gmail.com>
2025-12-26 12:39:43 -0800

    Update src\components\Footer\Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Data from California State Grants Portal •
+        Showing grants eligible for county government agencies •
+        Cache refreshes every 12 hours
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
