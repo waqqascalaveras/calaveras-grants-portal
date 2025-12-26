d8470dbf9a62239694863a9534a229172a386d66
waqqascalaveras <waqqascalaveras@gmail.com>
2025-12-26 11:18:15 -0800

    Update src\App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders Calaveras County Grants Portal header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Calaveras County Grants Portal/i);
-  expect(headerElement).toBeInTheDocument();
-});
-
 test('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<App />, div);
+  render(<App />);
 });
