41ee5d41be753726d206dc0e0d2e03c5a3e0f59c
waqqascalaveras <waqqascalaveras@gmail.com>
2025-12-26 15:15:06 -0800

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import CalaverrasGrantsDashboard from './components/CalaverrasGrantsDashboard';
+import UserTypeSelector from './components/UserTypeSelector';
+import DepartmentSelector from './components/DepartmentSelector';
 
 function App() {
-  return <CalaverrasGrantsDashboard />;
+  const [userType, setUserType] = useState(null);
+  const [subType, setSubType] = useState(null);
+
+  if (!userType) {
+    return <UserTypeSelector userType={userType} onUserTypeSelect={setUserType} />;
+  }
+  if (!subType) {
+    return <DepartmentSelector userType={userType} subType={subType} onSubTypeSelect={setSubType} />;
+  }
+
+  // Pass userType and subType as props if needed
+  return <CalaverrasGrantsDashboard userType={userType} subType={subType} />;
 }
 
 export default App;
