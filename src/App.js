330868e9a1888f87f95fc73576b53a5a6d0c38a0
waqqascalaveras <waqqascalaveras@gmail.com>
2025-12-26 10:51:03 -0800

    Add src\App.js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CalaverrasGrantsDashboard from './components/CalaverrasGrantsDashboard';
+
+function App() {
+  return <CalaverrasGrantsDashboard />;
+}
+
+export default App;
