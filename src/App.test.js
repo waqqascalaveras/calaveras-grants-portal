fcc8fe91a2f3fa8ca201c3fe2ce54a8414f755f2
Waqqas <waqqascalaveras@gmail.com>
2025-12-30 15:10:17 -0800

    Update 42 files
    Calaveras County Grants Discovery Portal

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,3 @@
-jest.mock('./components/CalaverrasGrantsDashboard', () => () => <div />);
-jest.mock('./components/UserTypeSelector', () => () => <div />);
-jest.mock('./components/DepartmentSelector', () => () => <div />);
-import { render } from '@testing-library/react';
-import App from './App';
 
 import { render } from '@testing-library/react';
 import App from './App';
